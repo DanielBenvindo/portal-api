a60466db5beca1cafb4b51b5180c6aa6836dcff9
bruno.costa <bruno.costa@compwire.com.br>
2022-06-26 19:09:08 -0300

    mais alguns ajustes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:nome, :cpf, :municipio, :uf])
+        #devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:nome, :email, :encrypted_password, :cpf, :municipio, :uf) }
+        #devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:nome, :email, :encrypted_password, :cpf, :municipio, :uf) }
+    end
 end
